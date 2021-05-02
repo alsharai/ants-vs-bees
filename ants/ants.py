1880808098ad5a1fe2039f5b65bd686235b265a1
Aiman Alsharai <alsharai.aiman@gmail.com>
2021-05-02 15:08:31 -0700

    Delete Comment
    Removed unnecessary comment

--- ants/ants.py
+++ ants/ants.py
@@ -485,7 +485,7 @@ class FireAnt(Ant):
     damage = 3
     "*** YOUR CODE HERE ***"
     implemented = True
-    food_cost = 4 #Do you need to have this since its already in the base class?
+    food_cost = 4
 
     def reduce_armor(self, amount):
         "*** YOUR CODE HERE ***"
